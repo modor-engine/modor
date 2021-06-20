0110492e416fb55dbf1c0c133d5113ddebfe7596
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2021-06-20 16:21:46 +0200

    Add unit tests for system iterators

--- src/external/systems/iterators.rs
+++ src/external/systems/iterators.rs
@@ -103,8 +103,8 @@ impl<'a, T> QueryIter<'a, T>
 where
     T: TupleSystemParam,
 {
-    pub(crate) fn new(iter: Query<'a, T>) -> Self {
-        QueryIter(iter)
+    pub(crate) fn new(query: Query<'a, T>) -> Self {
+        QueryIter(query)
     }
 }
 
@@ -125,6 +125,26 @@ mod option_component_iter_tests {
 
     assert_impl_all!(OptionComponentIter<'_, u32>: Sync, Send);
     assert_not_impl_any!(OptionComponentIter<'_, u32>: Clone);
+
+    #[test]
+    fn create_present_iter() {
+        let components = vec![10, 20];
+        let iter = Some(components.iter());
+
+        let component_iter = OptionComponentIter::<u32>::new(iter);
+
+        assert_iter!(component_iter, [Some(&10), Some(&20)]);
+    }
+
+    #[test]
+    fn create_missing_iter() {
+        let iter = None;
+
+        let mut component_iter = OptionComponentIter::<u32>::new(iter);
+
+        assert_eq!(component_iter.next(), Some(None));
+        assert_eq!(component_iter.next(), Some(None));
+    }
 }
 
 #[cfg(test)]
@@ -133,28 +153,129 @@ mod option_component_mut_iter_tests {
 
     assert_impl_all!(OptionComponentMutIter<'_, u32>: Sync, Send);
     assert_not_impl_any!(OptionComponentMutIter<'_, u32>: Clone);
+
+    #[test]
+    fn create_present_iter() {
+        let mut components = vec![10, 20];
+        let iter = Some(components.iter_mut());
+
+        let component_iter = OptionComponentMutIter::<u32>::new(iter);
+
+        assert_iter!(component_iter, [Some(&mut 10), Some(&mut 20)]);
+    }
+
+    #[test]
+    fn create_missing_iter() {
+        let iter = None;
+
+        let mut component_iter = OptionComponentMutIter::<u32>::new(iter);
+
+        assert_eq!(component_iter.next(), Some(None));
+        assert_eq!(component_iter.next(), Some(None));
+    }
 }
 
 #[cfg(test)]
 mod group_iter_tests {
     use super::*;
+    use crate::internal::main::MainFacade;
+    use crate::SystemOnceBuilder;
 
     assert_impl_all!(GroupIter<'_>: Sync, Send);
     assert_not_impl_any!(GroupIter<'_>: Clone);
+
+    #[test]
+    fn create_iter() {
+        let mut main = MainFacade::default();
+        let group_idx = main.create_group();
+        let entity_idx = main.create_entity(group_idx);
+        main.add_component(entity_idx, 10_u32);
+        main.run_system_once(SystemOnceBuilder::new(|data, _| {
+            let mut group_iter = GroupIter::new(group_idx, data.clone());
+
+            let group = group_iter.next();
+            assert!(group.is_some());
+            let mut group = group.unwrap();
+            group.delete();
+        }));
+        main.apply_system_actions();
+        main.run_system_once(SystemOnceBuilder::new(|data, _| {
+            assert_eq!(data.entity_idxs(0), []);
+        }));
+    }
 }
 
 #[cfg(test)]
 mod entity_iter_tests {
     use super::*;
+    use crate::internal::main::MainFacade;
+    use crate::SystemOnceBuilder;
 
     assert_impl_all!(EntityIter<'_>: Sync, Send);
     assert_not_impl_any!(EntityIter<'_>: Clone);
+
+    #[test]
+    fn create_iter() {
+        let mut main = MainFacade::default();
+        let group_idx = main.create_group();
+        let entity1_idx = main.create_entity(group_idx);
+        let entity2_idx = main.create_entity(group_idx);
+        let entity3_idx = main.create_entity(group_idx);
+        main.add_component(entity1_idx, 10_u32);
+        main.add_component(entity2_idx, 20_u32);
+        main.add_component(entity3_idx, 30_u32);
+        main.run_system_once(SystemOnceBuilder::new(|data, _| {
+            let components = &[0, 1, 2];
+            let mut entity_iter = EntityIter::new(components.iter(), data.clone());
+
+            let entity = entity_iter.next();
+            assert!(entity.is_some());
+            let mut entity = entity.unwrap();
+            entity.delete();
+            let entity = entity_iter.next();
+            assert!(entity.is_some());
+            let entity = entity_iter.next();
+            assert!(entity.is_some());
+            let mut entity = entity.unwrap();
+            entity.delete();
+        }));
+        main.apply_system_actions();
+        main.run_system_once(SystemOnceBuilder::new(|data, _| {
+            assert_eq!(data.entity_idxs(0), [1]);
+        }));
+    }
 }
 
 #[cfg(test)]
 mod query_iter_tests {
     use super::*;
+    use crate::internal::main::MainFacade;
+    use crate::SystemOnceBuilder;
+    use std::any::TypeId;
 
     assert_impl_all!(QueryIter<'_, (&u32, )>: Sync, Send);
     assert_not_impl_any!(QueryIter<'_, (&u32, )>: Clone);
+
+    #[test]
+    fn create_iter() {
+        let mut main = MainFacade::default();
+        let group_idx = main.create_group();
+        let entity1_idx = main.create_entity(group_idx);
+        let entity2_idx = main.create_entity(group_idx);
+        main.add_component(entity1_idx, 10_u32);
+        main.add_component(entity2_idx, 20_u32);
+        main.run_system_once(SystemOnceBuilder::new(|data, _| {
+            let mut query = Query::<(&u32,)>::new(data.clone(), Some(group_idx));
+            query.filter::<i64>();
+
+            let mut query_iter = QueryIter::new(query);
+
+            let query = query_iter.next();
+            assert!(query.is_some());
+            let query = query.unwrap();
+            let query_run = query.run(|_: &u32| ());
+            assert_eq!(query_run.group_idx, Some(group_idx));
+            assert_eq!(query_run.filtered_component_types, [TypeId::of::<i64>()]);
+        }));
+    }
 }
