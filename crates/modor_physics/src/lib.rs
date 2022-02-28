ae01f92ac7de8b49ac940281118451ec6b4acdbb
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2022-02-28 20:26:53 +0100

    Fix mutation tests

--- crates/modor_physics/src/lib.rs
+++ crates/modor_physics/src/lib.rs
@@ -1,1 +1,16 @@
 //! Physics module of modor.
+
+/// Temporary function
+pub fn f() -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::f;
+
+    #[test]
+    fn test() {
+        assert_eq!(f(), 0);
+    }
+}
