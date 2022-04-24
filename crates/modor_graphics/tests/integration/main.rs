50f4977710606271c5d0f52a17a5e55e26e9ba70
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2022-04-24 15:09:57 +0200

    Increase test coverage + optimize modor_graphics tests

---
+++ crates/modor_graphics/tests/integration/main.rs
@@ -0,0 +1,6 @@
+pub mod background;
+pub mod capture;
+pub mod hierarchy;
+pub mod shapes;
+pub mod testing;
+pub mod transparency;
--- crates/modor_graphics/tests/integration/testing.rs
+++ crates/modor_graphics/tests/integration/testing.rs
@@ -1,6 +1,24 @@
+use std::fs;
 use modor::testing::TestApp;
 use modor::App;
 use modor_graphics::{testing, BackgroundColor, Capture, Color, GraphicsModule, SurfaceSize};
+use std::path::Path;
+
+#[test]
+fn capture_save_if_not_existing() {
+    let mut app: TestApp = App::new()
+        .with_entity(GraphicsModule::build_windowless())
+        .with_entity(Capture::build(SurfaceSize::new(300, 200)))
+        .with_entity(BackgroundColor::build(Color::GREEN))
+        .into();
+    app.update();
+    testing::assert_capture(&app, "tests/expected/testing.png");
+    let written_file_path = Path::new("tests/expected/testing_write.png");
+    testing::assert_capture(&app, written_file_path);
+    let file_exists = written_file_path.exists();
+    let _ = fs::remove_file(written_file_path);
+    assert!(file_exists);
+}
 
 #[test]
 #[should_panic]
