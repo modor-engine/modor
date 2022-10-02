ac23d418f3b2916861e03d644865d8ca49647aff
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2022-10-02 19:32:05 +0200

    Debug issue with registration check script

--- .github/workflows/scripts/check_registered_crates.sh
+++ .github/workflows/scripts/check_registered_crates.sh
@@ -5,6 +5,11 @@ cargo update
 for crate_path in crates/*; do
     crate_name=$(basename "$crate_path")
     cargo search --limit 5
+    cargo search modor --limit 1
+    (cargo search a --limit 1 | grep "^a = ")\
+        || (echo "Crate a not registered on crates.io" && exit 1)
+    (cargo search modor --limit 1 | grep "^modor = ")\
+        || (echo "Crate modor not registered on crates.io" && exit 1)
     (cargo search "$crate_name" --limit 1 | grep "^$crate_name = ")\
         || (echo "Crate '$crate_name' not registered on crates.io" && exit 1)
 done
