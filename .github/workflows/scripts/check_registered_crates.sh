aee82ac9091ce1944588fcf22c67f5091e0a1b4c
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2022-10-03 17:41:00 +0200

    Fix issue with registration check script

--- .github/workflows/scripts/check_registered_crates.sh
+++ .github/workflows/scripts/check_registered_crates.sh
@@ -3,10 +3,6 @@ set -xeu
 
 for crate_path in crates/*; do
     crate_name=$(basename "$crate_path")
-    if cargo search "$crate_name" --limit 1 | grep "^$crate_name = "; then
-        echo "'$crate_name' already registered on crates.io"
-    else
-        echo "Crate '$crate_name' not registered on crates.io"
-        exit 1
-    fi
+    cargo search "$crate_name" --limit 1 | grep "^$crate_name = " \
+        || (echo "Crate '$crate_name' not registered on crates.io" && exit 1)
 done
