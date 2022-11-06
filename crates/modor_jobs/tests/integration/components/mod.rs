6512ed65cb5260dfc779399e668fa47ddec98a64
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2022-11-06 21:05:09 +0100

    Fix tests for WASM

--- crates/modor_jobs/tests/integration/components/mod.rs
+++ crates/modor_jobs/tests/integration/components/mod.rs
@@ -1,4 +1,2 @@
-#[cfg(not(target_arch = "wasm32"))]
 pub mod asset_loading;
-#[cfg(not(target_arch = "wasm32"))]
 pub mod job;
--- crates/modor_jobs/tests/integration/main.rs
+++ crates/modor_jobs/tests/integration/main.rs
@@ -1,4 +1,5 @@
 #![allow(clippy::unwrap_used)]
+#![cfg(not(target_arch = "wasm32"))]
 
 #[macro_use]
 extern crate modor;
