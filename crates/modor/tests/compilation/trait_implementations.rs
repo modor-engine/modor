cc8fad5fa6a96cc6888dbe9ebb3192b351657a87
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2022-11-15 21:17:48 +0100

    Add `Or` entity filter + optimize physics module system

--- crates/modor/tests/compilation/trait_implementations.rs
+++ crates/modor/tests/compilation/trait_implementations.rs
@@ -1,5 +1,6 @@
 use modor::{
-    App, ChildBuilder, DependsOn, Entity, EntityBuilder, Query, Single, SingleMut, With, World,
+    App, ChildBuilder, DependsOn, Entity, EntityBuilder, Filter, Or, Query, Single, SingleMut,
+    With, World,
 };
 use std::panic::{RefUnwindSafe, UnwindSafe};
 
@@ -15,8 +16,10 @@ assert_impl_all!(App: Send, Unpin);
 assert_impl_all!(DependsOn<Action>: Sync, Send, UnwindSafe, RefUnwindSafe, Unpin);
 assert_impl_all!(ChildBuilder<'_>: Send, Unpin);
 assert_impl_all!(Entity<'_>: Sync, Send, Unpin);
-assert_impl_all!(EntityBuilder<Singleton, (), ()>:  Sync, Send, UnwindSafe, RefUnwindSafe, Unpin);
+assert_impl_all!(EntityBuilder<Singleton, (), ()>: Sync, Send, UnwindSafe, RefUnwindSafe, Unpin);
+assert_impl_all!(Filter<With<u32>>: Sync, Send, UnwindSafe, RefUnwindSafe, Unpin);
 assert_impl_all!(Query<'_, ()>: Sync, Send, Unpin);
+assert_impl_all!(Or<With<u32>>: Sync, Send, UnwindSafe, RefUnwindSafe, Unpin);
 assert_impl_all!(Single<'_, Singleton>: Sync, Send, Unpin);
 assert_impl_all!(SingleMut<'_, Singleton>: Sync, Send, Unpin);
 assert_impl_all!(With<u32>: Sync, Send, UnwindSafe, RefUnwindSafe, Unpin);
