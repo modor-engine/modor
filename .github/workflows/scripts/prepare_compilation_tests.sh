f3239df3ea2eea0e6adfe0ff1c4a392f6e1e46c3
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2023-06-13 18:53:40 +0200

    Fix CI

--- .github/workflows/scripts/prepare_compilation_tests.sh
+++ .github/workflows/scripts/prepare_compilation_tests.sh
@@ -2,4 +2,5 @@
 set -xeu
 
 # Used to avoid failures of compilation tests because of multiple compiled versions of the same library
-rm -rf target/debug/deps/*modor*
+rm -rf target/debug/*/*modor*
+rm -rf target/debug/.*/*modor*
