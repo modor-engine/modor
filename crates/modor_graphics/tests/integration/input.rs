8a69ed98b0352ba8847f0342e49de0eb186b6a29
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2023-09-24 13:09:57 +0200

    Fix modor_graphics tests

--- crates/modor_graphics/tests/integration/input.rs
+++ crates/modor_graphics/tests/integration/input.rs
@@ -1,4 +1,4 @@
-use modor::{App, With};
+use modor::{App, SingleRef, With};
 use modor_graphics::testing;
 use modor_graphics::testing::{TestRunnerContext, UpdateState};
 use modor_input::{Fingers, Key, Keyboard, Mouse, MouseButton};
@@ -41,39 +41,49 @@ fn press_mouse_button(context: &mut TestRunnerContext) {
 }
 
 fn move_mouse_wheel_in_lines(context: &mut TestRunnerContext) {
-    App::new().with_entity(modor_input::module()).run(|a| {
-        testing::test_runner(a, context, 2, |s| {
-            if s.update_id == 0 {
-                let delta = MouseScrollDelta::LineDelta(2., 3.);
-                s.next_events.push(mouse_wheel_event(&s, delta));
-                s.app
-            } else {
-                s.app.assert::<With<Mouse>>(1, |e| {
-                    e.has(|m: &Mouse| {
-                        assert_approx_eq!(m.scroll_delta.as_lines(100., 200.), Vec2::new(2., -3.));
+    App::new()
+        .with_entity(modor_input::module())
+        .with_entity(MouseTest::new(1, |m| {
+            assert_approx_eq!(m.scroll_delta.as_lines(100., 200.), Vec2::new(2., -3.));
+        }))
+        .run(|a| {
+            testing::test_runner(a, context, 2, |s| {
+                if s.update_id == 0 {
+                    let delta = MouseScrollDelta::LineDelta(2., 3.);
+                    s.next_events.push(mouse_wheel_event(&s, delta));
+                    s.app
+                } else {
+                    s.app.assert::<With<Mouse>>(1, |e| {
+                        e.has(|m: &Mouse| {
+                            assert_approx_eq!(m.scroll_delta.as_lines(100., 200.), Vec2::ZERO);
+                        })
                     })
-                })
-            }
+                }
+            });
         });
-    });
 }
 
 fn move_mouse_wheel_in_pixels(context: &mut TestRunnerContext) {
-    App::new().with_entity(modor_input::module()).run(|a| {
-        testing::test_runner(a, context, 2, |s| {
-            if s.update_id == 0 {
-                let delta = MouseScrollDelta::PixelDelta(PhysicalPosition::new(2., 3.));
-                s.next_events.push(mouse_wheel_event(&s, delta));
-                s.app
-            } else {
-                s.app.assert::<With<Mouse>>(1, |e| {
-                    e.has(|m: &Mouse| {
-                        assert_approx_eq!(m.scroll_delta.as_pixels(100., 200.), Vec2::new(2., -3.));
+    App::new()
+        .with_entity(modor_input::module())
+        .with_entity(MouseTest::new(1, |m| {
+            assert_approx_eq!(m.scroll_delta.as_pixels(100., 200.), Vec2::new(2., -3.));
+        }))
+        .run(|a| {
+            testing::test_runner(a, context, 2, |s| {
+                if s.update_id == 0 {
+                    let delta = MouseScrollDelta::PixelDelta(PhysicalPosition::new(2., 3.));
+                    s.next_events.push(mouse_wheel_event(&s, delta));
+                    s.app
+                } else {
+                    s.app.assert::<With<Mouse>>(1, |e| {
+                        e.has(|m: &Mouse| {
+                            assert_approx_eq!(m.scroll_delta.as_pixels(100., 200.), Vec2::ZERO);
+                        })
                     })
-                })
-            }
+                }
+            });
         });
-    });
 }
 
 fn move_mouse(context: &mut TestRunnerContext) {
@@ -93,19 +103,24 @@ fn move_mouse(context: &mut TestRunnerContext) {
 }
 
 fn detect_mouse_motion(context: &mut TestRunnerContext) {
-    App::new().with_entity(modor_input::module()).run(|a| {
-        testing::test_runner(a, context, 2, |s| {
-            if s.update_id == 0 {
-                s.next_events.push(mouse_motion_event((1., 2.)));
-                s.next_events.push(mouse_motion_event((3., 1.)));
-                s.app
-            } else {
-                s.app.assert::<With<Mouse>>(1, |e| {
-                    e.has(|m: &Mouse| assert_eq!(m.delta, Vec2::new(4., -3.)))
-                })
-            }
+    App::new()
+        .with_entity(modor_input::module())
+        .with_entity(MouseTest::new(1, |m| {
+            assert_approx_eq!(m.delta, Vec2::new(4., -3.));
+        }))
+        .run(|a| {
+            testing::test_runner(a, context, 2, |s| {
+                if s.update_id == 0 {
+                    s.next_events.push(mouse_motion_event((1., 2.)));
+                    s.next_events.push(mouse_motion_event((3., 1.)));
+                    s.app
+                } else {
+                    s.app.assert::<With<Mouse>>(1, |e| {
+                        e.has(|m: &Mouse| assert_approx_eq!(m.delta, Vec2::ZERO))
+                    })
+                }
+            });
         });
-    });
 }
 
 fn press_keyboard_key(context: &mut TestRunnerContext) {
@@ -123,60 +138,72 @@ fn press_keyboard_key(context: &mut TestRunnerContext) {
 }
 
 fn enter_character(context: &mut TestRunnerContext) {
-    App::new().with_entity(modor_input::module()).run(|a| {
-        testing::test_runner(a, context, 2, |s| {
-            if s.update_id == 0 {
-                s.next_events.push(received_character_event(&s, 'A'));
-                s.next_events.push(received_character_event(&s, 'B'));
-                s.app
-            } else {
-                s.app
-                    .assert::<With<Keyboard>>(1, |e| e.has(|k: &Keyboard| assert_eq!(k.text, "AB")))
-            }
+    App::new()
+        .with_entity(modor_input::module())
+        .with_entity(KeyboardTest::new(1, |k| assert_eq!(k.text, "AB")))
+        .run(|a| {
+            testing::test_runner(a, context, 2, |s| {
+                if s.update_id == 0 {
+                    s.next_events.push(received_character_event(&s, 'A'));
+                    s.next_events.push(received_character_event(&s, 'B'));
+                    s.app
+                } else {
+                    s.app.assert::<With<Keyboard>>(1, |e| {
+                        e.has(|k: &Keyboard| assert_eq!(k.text, ""))
+                    })
+                }
+            });
         });
-    });
 }
 
 fn touch_screen(context: &mut TestRunnerContext) {
-    App::new().with_entity(modor_input::module()).run(|a| {
-        testing::test_runner(a, context, 4, |s| match s.update_id {
-            0 => {
-                let position = PhysicalPosition::new(4., 8.);
-                s.next_events
-                    .push(touch_event(&s, 42, position, TouchPhase::Started));
-                s.app
-            }
-            1 => {
-                let position = PhysicalPosition::new(5., 8.);
-                s.next_events
-                    .push(touch_event(&s, 42, position, TouchPhase::Moved));
-                s.app.assert::<With<Fingers>>(1, |e| {
-                    e.has(|f: &Fingers| {
-                        assert!(f[42].state.is_pressed());
-                        assert_approx_eq!(f[42].position, Vec2::new(4., 8.));
-                        assert_approx_eq!(f[42].delta, Vec2::ZERO);
+    App::new()
+        .with_entity(modor_input::module())
+        .with_entity(FingersTest::new(1, |f| {
+            assert_approx_eq!(f[42].delta, Vec2::ZERO);
+        }))
+        .with_entity(FingersTest::new(2, |f| {
+            assert_approx_eq!(f[42].delta, Vec2::new(1., 0.));
+        }))
+        .run(|a| {
+            testing::test_runner(a, context, 4, |s| match s.update_id {
+                0 => {
+                    let position = PhysicalPosition::new(4., 8.);
+                    s.next_events
+                        .push(touch_event(&s, 42, position, TouchPhase::Started));
+                    s.app
+                }
+                1 => {
+                    let position = PhysicalPosition::new(5., 8.);
+                    s.next_events
+                        .push(touch_event(&s, 42, position, TouchPhase::Moved));
+                    s.app.assert::<With<Fingers>>(1, |e| {
+                        e.has(|f: &Fingers| {
+                            assert!(f[42].state.is_pressed());
+                            assert_approx_eq!(f[42].position, Vec2::new(4., 8.));
+                            assert_approx_eq!(f[42].delta, Vec2::ZERO);
+                        })
                     })
-                })
-            }
-            2 => {
-                let position = PhysicalPosition::new(5., 8.);
-                s.next_events
-                    .push(touch_event(&s, 42, position, TouchPhase::Ended));
-                s.app.assert::<With<Fingers>>(1, |e| {
-                    e.has(|f: &Fingers| {
-                        assert!(f[42].state.is_pressed());
-                        assert_approx_eq!(f[42].position, Vec2::new(5., 8.));
-                        assert_approx_eq!(f[42].delta, Vec2::new(1., 0.));
+                }
+                2 => {
+                    let position = PhysicalPosition::new(5., 8.);
+                    s.next_events
+                        .push(touch_event(&s, 42, position, TouchPhase::Ended));
+                    s.app.assert::<With<Fingers>>(1, |e| {
+                        e.has(|f: &Fingers| {
+                            assert!(f[42].state.is_pressed());
+                            assert_approx_eq!(f[42].position, Vec2::new(5., 8.));
+                            assert_approx_eq!(f[42].delta, Vec2::ZERO);
+                        })
                     })
-                })
-            }
-            _ => s.app.assert::<With<Fingers>>(1, |e| {
-                e.has(|f: &Fingers| {
-                    assert!(!f[42].state.is_pressed());
-                })
-            }),
+                }
+                _ => s.app.assert::<With<Fingers>>(1, |e| {
+                    e.has(|f: &Fingers| {
+                        assert!(!f[42].state.is_pressed());
+                    })
+                }),
+            });
         });
-    });
 }
 
 fn suspend_app(context: &mut TestRunnerContext) {
@@ -299,5 +326,83 @@ fn touch_event(
             force: None,
             id,
         }),
+    }
+}
+
+#[derive(Component)]
+struct MouseTest {
+    current_update_id: usize,
+    test_update_id: usize,
+    test: fn(&Mouse),
+}
+
+#[systems]
+impl MouseTest {
+    fn new(test_update_id: usize, test: fn(&Mouse)) -> Self {
+        Self {
+            current_update_id: 0,
+            test_update_id,
+            test,
+        }
+    }
+
+    #[run]
+    fn update(&mut self, mouse: SingleRef<'_, '_, Mouse>) {
+        if self.current_update_id == self.test_update_id {
+            (self.test)(mouse.get());
+        }
+        self.current_update_id += 1;
+    }
+}
+
+#[derive(Component)]
+struct KeyboardTest {
+    current_update_id: usize,
+    test_update_id: usize,
+    test: fn(&Keyboard),
+}
+
+#[systems]
+impl KeyboardTest {
+    fn new(test_update_id: usize, test: fn(&Keyboard)) -> Self {
+        Self {
+            current_update_id: 0,
+            test_update_id,
+            test,
+        }
+    }
+
+    #[run]
+    fn update(&mut self, keyboard: SingleRef<'_, '_, Keyboard>) {
+        if self.current_update_id == self.test_update_id {
+            (self.test)(keyboard.get());
+        }
+        self.current_update_id += 1;
+    }
+}
+
+#[derive(Component)]
+struct FingersTest {
+    current_update_id: usize,
+    test_update_id: usize,
+    test: fn(&Fingers),
+}
+
+#[systems]
+impl FingersTest {
+    fn new(test_update_id: usize, test: fn(&Fingers)) -> Self {
+        Self {
+            current_update_id: 0,
+            test_update_id,
+            test,
+        }
+    }
+
+    #[run]
+    fn update(&mut self, fingers: SingleRef<'_, '_, Fingers>) {
+        if self.current_update_id == self.test_update_id {
+            (self.test)(fingers.get());
+        }
+        self.current_update_id += 1;
     }
 }
